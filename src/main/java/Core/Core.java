4f312c96487f6abd7966bb481f51a88beef3fd8d
LABMATT <matt42764@gmail.com>
2021-08-12 02:54:57 +1000

    JDBC
    - Setup JDBC manger.

--- src/main/java/Core/Core.java
+++ src/main/java/Core/Core.java
@@ -1,8 +1,29 @@
 package Core;
 
+import org.java_websocket.server.WebSocketServer;
+import socketManger.SocketManger;
+import sqlManger.PStatment;
+
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Core {
 
     public static void main(String[] args) {
         System.out.println("LABMATT DOLUDE SERVER SYSTEMS");
+
+        String host = "localhost";
+        int port = 8080;
+
+        new PStatment().getQury();
+
+        WebSocketServer server = new SocketManger(new InetSocketAddress(host, port));
+        server.run();
+
+
     }
 }
+
