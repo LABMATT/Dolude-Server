aa9f24a22b7944b569c89386d2cbf9ca6b5f26f6
LABMATT <matt42764@gmail.com>
2021-08-19 02:20:57 +1000

    Santize input
    - Can decode messages into prefix and sufixs.
    - Can santize input by max length and by SQL input protection.
    - Reponse error to client.

--- src/main/java/MessageManger/Message.java
+++ src/main/java/MessageManger/Message.java
@@ -2,7 +2,7 @@ package MessageManger;
 
 import org.java_websocket.WebSocket;
 
-public class Message implements Runnable{
+public class Message implements Runnable {
 
     // The recived message on initinalation.
     private final String message;
@@ -19,27 +19,36 @@ public class Message implements Runnable{
     @Override
     public void run() {
 
-        final int prefixpos = message.indexOf(':');
+        // Makes sure there is a command sep.
+        if (message.contains(":")) {
 
-        String prefix = message.substring(0, prefixpos);
-        String sufix = message.substring(prefixpos + 1);
+            final int prefixpos = message.indexOf(':');
 
-        try {
-            sufix = new Sanitizer().Sanitizer(sufix, 10);
+            String prefix = message.substring(0, prefixpos);
+            String sufix = message.substring(prefixpos + 1);
+
+            try {
+                prefix = new Sanitizer().sanitize(prefix, 20);
+                sufix = new Sanitizer().sanitize(sufix, 20);
 
 
+            } catch (Exception e) {
+                connection.send(prefix + ": Input unsuitable for use <" + e.getMessage() + ">");
+            }
 
-        } catch (Exception e) {
-            connection.send(prefix + ": Input unsuitable for use." );
+            System.out.println("prefix: " + prefix);
+            System.out.println("suffix: " + sufix);
+
+            System.out.println("the thread ran");
+
+
+            System.out.println("The message is: " + message);
+
+        } else
+        {
+            connection.send("Input unsuitable for use <Missing Prefix>");
         }
 
-        System.out.println("prefix: " + prefix);
-        System.out.println("suffix: " + sufix);
-
-        System.out.println("the thread ran");
-
-
-        System.out.println("The message is: " + message);
 
     }
 }
